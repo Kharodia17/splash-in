ea9ebcfb902d156f7b05d575323f7de30f647f12
Kharodia17 <kharodia8@gmail.com>
2025-08-15 20:41:27 +0200

    update phoneNumber

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openWhatsApp() {
     // Replace with your actual phone number in international format without the +
-    const phoneNumber = "27682715293"; 
+    const phoneNumber = "27827550575"; 
     const message = encodeURIComponent("Hello! I'm interested in your swimming classes.");
     window.open(`https://wa.me/${phoneNumber}?text=${message}`, "_blank");
 }
